6d47e47a67403a69af04ed25c8786473ba3914e0
Hwang Tae Young <hty3466@naver.com>
2022-12-05 15:55:57 +0900

    [feat] webpack 설정 완료

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const App = () => {
+  return <>리액트 웹팩 없이 완료!!</>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "index.js",
+    clean: true,
+  },
+
+  devServer: {
+    port: 3000,
+    liveReload: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+};
+// 아래의 것은 맨처음에 했던 것, 동작 잘 되는중!
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const path = require("path");
+
+// module.exports = {
+//   mode: "development",
+//   entry: "./src/index.js",
+//   resolve: {
+//     extensions: [".tsx", ".ts", "..."],
+//   },
+//   devServer: {
+//     port: 3000,
+//     liveReload: true,
+//   },
+//   output: { filename: "build.js", clean: true },
+//   plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+//   module: {
+//     rules: [{ test: /\.(js|jsx|ts|tsx)$/, use: "babel-loader" }],
+//   },
+// };
